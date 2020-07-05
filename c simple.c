7048589437faf3a450e1c14b03562778f26c0cbf
pratyush19919 <52246459+pratyush19919@users.noreply.github.com>
2020-07-05 16:32:36 +0530

    Add files via upload

---
+++ c simple.c
@@ -0,0 +1,105 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+// int arr[7]={10,20,30,40,50,60,100};
+// int sumarr(int a[],int size){
+//     int sum=0;
+//     for (int i=0;i<size;i++){
+//         sum=sum+a[i];
+        
+//     }
+//     return sum;
+// }
+// int main()
+// {
+    
+    
+   
+//     printf("Sum of array is %d",sumarr(arr,7));
+//     return 0;
+//}
+// int main(){
+//     int x=30;
+//     float r=128736910.3773478913;
+//     printf("Addrress of x is %x and r is %x",&x,&r);
+// }
+// int main(){
+//     int *r=6324;
+//     printf("%x\n",&r);
+//     int *ptr;
+//     ptr = &r;
+//     printf("%d\n",*ptr);
+//     printf("%x\n",*r);
+//     return 0;
+// }
+// int main(){
+//     int arr[6]={12,443,54,23,14,100};
+//     int *arr_ptr[6];
+//     for (int i=0;i<6;i++){
+//         arr_ptr[i]=&arr[i];
+//     }
+//     for (int i=0;i<6;i++){
+//     printf("val of arr_ptr[%d] is %d\n",i,arr_ptr[i]);
+//     printf("val of arr[%d] is %d\n",i,arr[i]);//these two are same,direct array print
+//     printf("val of dereferenced arr_ptr[%d] is %d\n",i,*arr_ptr[i]);//these two are same array print using dereferencing
+//     }
+//     return 0;
+// }
+
+// void getval(int *ptr){
+    
+//     *ptr=10000;
+//     return;
+// }
+// int main(){
+//     int val;
+//     getval(&val);
+//     printf("address of val is changed to %d using getval function",val);//address of val=*ptr10000
+    
+//     return 0;
+    
+// }
+
+//sum of elements in array using pointers
+
+// int sumofarr(int *ptr[], int size){
+//     int sum=0;
+//     for (int i=0;i<size;i++){
+//         sum=sum + *ptr[i]; 
+//     }
+//   return sum;
+// }
+
+
+// int main(){
+//     int arr[6]={12,443,54,23,14,100};
+//     int*ptr_arr_of_arr[6];
+//     for(int i=0;i<6;i++){
+//         ptr_arr_of_arr[i]=&arr[i];
+//     }
+//     // for(int i=0;i<6;i++){
+//     // printf("address of element in array arr[%d] is %d \n",i,ptr_arr_of_arr[i]);}
+//     printf("sum of array using pointers as arguments to function is %d", sumofarr(ptr_arr_of_arr,6));
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
